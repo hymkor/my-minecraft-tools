388c1a4bbffafcb42af9100e3ab9573538a93d4f
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-11-02 17:47:22 +0900

    Backup-minecraft.cmd: exclude backups and mods folders from backup

--- Backup-minecraft.cmd
+++ Backup-minecraft.cmd
@@ -1,7 +1,14 @@
 @setlocal
 @set PROMPT=$D$S$T$G$S
+
+rem (e.g.) set "RESTIC_REPOSITORY=%USERPROFILE%\マイドライブ\restic"
+
 pushd "%APPDATA%"
-restic.exe backup .minecraft  || exit /b 1
+restic.exe backup ^
+    --exclude ".minecraft/backups/*" ^
+    --exclude ".minecraft/mods/*" ^
+    .minecraft || exit /b 1
+pause
 restic.exe check  --read-data || exit /b 1
 restic.exe forget --keep-last 3 --prune
 popd
