8d62b40c1b24a53b00e47e0503156a29b48ddfbe
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-12-20 23:46:22 +0900

    Backup-minecraft.cmd: `pause` to `timeout 300`

--- Backup-minecraft.cmd
+++ Backup-minecraft.cmd
@@ -8,7 +8,7 @@ restic.exe backup ^
     --exclude ".minecraft/backups/*" ^
     --exclude ".minecraft/mods/*" ^
     .minecraft || exit /b 1
-pause
+timeout 300
 restic.exe check  --read-data || exit /b 1
 restic.exe forget --keep-last 2 --prune
 popd
